a902944d1f2d02a65bbdd807cf67ca5cfbe45d8b
EvgenyKMT <evgeny@kmt.co.il>
2024-08-12 12:39:52 +0300

    TODO list

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,20 @@ import { Options, OptionsAction } from "./options/types.ts";
 import "@mantine/core/styles.css";
 import "./style.scss";
 
+//TODO:
+// 1. Opacity and isActive props for Labels
+// 2. Multi rules for one label
+// 3. Default labels (for first launch)
+// 4. Label preview in edit form
+// 5. Import/export setting from file
+// 6. Import setting from url
+// 6.1 By user action (sync button)
+// 6.2 By timer (autoupdate)
+// 7. Draggable labels in the list
+// 8. UI improvements:
+// 8.1 user friendly position selector in the edit form,
+// 8.2 ribbon/triangle icons for labels in the edit form and in the labels' badges
+
 function App() {
   const {
     state: options,
