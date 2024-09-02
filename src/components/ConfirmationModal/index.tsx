8b67e80e02ffd9b1340c340753c62e70a734bb1f
evgeny <e.nefyodoff@gmail.com>
2024-09-02 22:42:12 +0300

    Confirmation dialog on delete

--- src/components/ConfirmationModal/index.tsx
+++ src/components/ConfirmationModal/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Group, Stack, Text } from "@mantine/core";
 import { ConfirmationModalProps } from "./types.ts";
+import { IconCheck, IconX } from "@tabler/icons-react";
 
 function ConfirmationModal({
   message,
@@ -10,10 +11,19 @@ function ConfirmationModal({
     <Stack>
       <Text size="sm">{message || "Are you sure?"}</Text>
       <Group gap="xs" justify="end">
-        <Button size="xs" onClick={onConfirm}>
+        <Button
+          size="xs"
+          onClick={onConfirm}
+          leftSection={<IconCheck size={14} />}
+        >
           Confirm
         </Button>
-        <Button size="xs" variant="light" color="dark" onClick={onClose}>
+        <Button
+          size="xs"
+          variant="light"
+          onClick={onClose}
+          leftSection={<IconX size={14} />}
+        >
           Cancel
         </Button>
       </Group>
